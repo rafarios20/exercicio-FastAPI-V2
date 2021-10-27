db99270965d97b7349c19c399a2337d1ce24c868
Rafael Rios <85204960+rafarios20@users.noreply.github.com>
2021-10-27 18:07:12 -0300

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-from tomatic import Tomatic
-from tomatic.buckets import EnvironBucket
-
-t = Tomatic(EnvironBucket, static_profile="FASTAPI", raise_if_none=ValueError)
-
-SQLALCHEMY_DATABASE_URL = "sqlite:///db.sqlite3"
-
-SQLALCHEMY_DATABASE_ARGS = {"check_same_thread": False}
